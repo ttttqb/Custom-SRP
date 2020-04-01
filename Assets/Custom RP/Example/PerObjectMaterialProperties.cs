109a1142e7744e47d51e026dd5cfa829cde4afc9
ttttqb <tanqinbing@hotmail.com>
2020-04-01 10:01:07 +0800

    Finish draw calls.

--- Assets/Custom RP/Example/PerObjectMaterialProperties.cs
+++ Assets/Custom RP/Example/PerObjectMaterialProperties.cs
@@ -7,6 +7,10 @@ public class PerObjectMaterialProperties : MonoBehaviour {
 	// ReSharper disable once InconsistentNaming
 	static int baseColorId = Shader.PropertyToID("_BaseColor");
 	private static MaterialPropertyBlock block;
+	static int cutoffId = Shader.PropertyToID("_Cutoff");
+	
+	[SerializeField, Range(0f, 1f)]
+	float cutoff = 0.5f;
 	
     [SerializeField] private Color baseColor = Color.white;
 
@@ -17,6 +21,7 @@ public class PerObjectMaterialProperties : MonoBehaviour {
 		    block = new MaterialPropertyBlock();
 	    }
 	    block.SetColor(baseColorId, baseColor);
+	    block.SetFloat(cutoffId, cutoff);
 	    GetComponent<Renderer>().SetPropertyBlock(block);
     }
 
